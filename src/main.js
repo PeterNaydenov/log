467175335f4b356471046e10011363c3e66542cc
Peter Naydenov <peter.naydenov@gmail.com>
2025-01-25 18:37:54 +0200

    jsDoc descriptions

--- src/main.js
+++ src/main.js
@@ -26,7 +26,23 @@ function cl ({ message, level, type, logLevel }) {
 } // cl func.
 
 
+/**
+ * @typedef LogOptions
+ * @type {object}
+ * @property {number|string[]} [defaultMessageLevel] - Default message level or array of possible values
+ * @property {string} [message] - Message to log
+ * @property {string} [type] - Optional. Type of the message: 'error', 'warning', or 'log'
+ * @property {number} [level] - Optional. Activity level of this specific message. Default is 1.
+ */
 
+
+
+/**
+ * Create a log function
+ * @param {Record<string, unknown>|LogOptions} [options={}] - Options. 
+ * @param {function} [logFunction=cl] - Custom log function if needed. Default is to log in the console(console.log).
+ * @returns {function} A function that excepts a log-object and returns the result of logFunction.
+ */
 function createLog ( options={}, logFunction=cl ) {
 return function log ({ message, type, level, ...rest}) {
         const 
