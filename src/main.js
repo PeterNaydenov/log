b8dda50f1a07f7032ef361eacea99494b67f239e
Peter Naydenov <peter.naydenov@gmail.com>
2025-01-27 08:24:52 +0200

    jsDoc description was added

--- src/main.js
+++ src/main.js
@@ -7,6 +7,29 @@
 
 
 
+
+
+/**
+ * @typedef LogOptions
+ * @type {object}
+ * @property {number|string[]} [defaultMessageLevel] - Default message level or array of possible values
+ * @property {string} [message] - Message to log
+ * @property {string} [type] - Optional. Type of the message: 'error', 'warning', or 'log'
+ * @property {number} [level] - Optional. Activity level of this specific message. Default is 1.
+ */
+
+
+
+/**
+ * Default log function. Uses console.log().
+ *
+ * @param {Object} params - The parameters for logging.
+ * @param {string} params.message - The message to log.
+ * @param {number} params.level - The level of the message.
+ * @param {string} params.type - The type of the message ('warn', 'error', or other).
+ * @param {number} params.logLevel - The log level that determines if the message should be logged.
+ * @returns {string|null} The formatted log message, or null if the message is not logged.
+ */
 function cl ({ message, level, type, logLevel }) {
 // *** Default log method -> log in console.
         if ( logLevel === 0   )   return null   // logLevel '0' in this function means "stay silent"; 
@@ -26,14 +49,7 @@ function cl ({ message, level, type, logLevel }) {
 } // cl func.
 
 
-/**
- * @typedef LogOptions
- * @type {object}
- * @property {number|string[]} [defaultMessageLevel] - Default message level or array of possible values
- * @property {string} [message] - Message to log
- * @property {string} [type] - Optional. Type of the message: 'error', 'warning', or 'log'
- * @property {number} [level] - Optional. Activity level of this specific message. Default is 1.
- */
+
 
 
 
